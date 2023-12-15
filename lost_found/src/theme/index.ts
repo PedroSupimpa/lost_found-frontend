e4e256e2ee637c4eccd66cc25a67df22e6ab8dc0
Pedro Souza <shiro2303@gmail.com>
2023-12-15 20:55:26 -0300

    Add theme configuration file

---
+++ lost_found/src/theme/index.ts
@@ -0,0 +1,18 @@
+const THEME = {
+
+    colors: {
+        primary: '#8234E9',
+        secondary: '#7554f6',
+        black: '#09090A',
+        white: '#FFFFFF',
+        card: 'your_card_color',
+        text: 'your_text_color',
+        border: 'your_border_color',
+        notification: 'your_notification_color',
+    },
+
+
+
+};
+
+export default THEME;
